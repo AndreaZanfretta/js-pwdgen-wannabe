f64f2d9b0fbb130c270060699309a6ec2db3b0b0
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-03-23 15:43:15 +0100

    esercizio finito

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* https://mermaid.live/edit#pako:eNp1kr1uwjAQx1_F8hwqtWOGSkAoIMEE6hIzXOMjGBzbsh2qFvFgnftitQ1BqCjbRf79P87xiVaaI81pbcHsyLpgau_IYPA6ZGpYFqLaCbDwLbRCcgQr4ENIJEo3uIkUGZW8h6l0nTCmRokc97pVWmobwXECi3LoHNbqxsVTAimUeAuN8EiM1Y3xQVMkzaRHcy3xKJsk2VuvLA0Pqrekmv7bJCjuXUBKuNvOhMNPbfnNTXAiZPQ0VugQpTyq1mtixO9PWjLramddESQvzyF-muKXTDE1Wy8X6WsW5nJs8RruvG29by1cfs-8rMJRcCjm7zEqhneFguE8QYtSCnWIVCQ81MRVVhhPnK1iVaWDqybbuM3T3m1i_iV8Wa5AHiE19Oh8nKS-yjc0ow3aBgQP7-vEFCGM-h02yGgeRg72wChT58C1hoPHCRdeW5pvQTrMKIRbWX2piuZhKeygQkB4q82VOv8BHwD4EA */
+
+let nome;
+let cognome;
+let colore;
+
+nome = prompt("Come ti chiami?");
+surname = prompt("Qual è il tuo cognome?");
+color = prompt("Qual è il colore preferito?");
+
+let password = document.getElementById("password").innerHTML += nome + cognome + colore + "21";
